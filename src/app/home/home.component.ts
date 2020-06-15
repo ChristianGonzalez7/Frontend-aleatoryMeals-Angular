b8100b1ef315e9710e36c1a5c80cbb8c104dd8a8
ChristianGonzalez7 <christiangonzalez1994@gmail.com>
2020-06-15 16:38:59 +0200

    onclick event added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,22 +8,45 @@ import { ServiceService } from '../service.service';
 })
 export class HomeComponent implements OnInit {
 
-  foodObject: any;
   randomMeal: any;
   food: string;
   mealName: string;
   ingredients: any[];
   method: any[];
+  nmbr: number;
+  displayMeal: string;
 
-  constructor(private serviceService: ServiceService) {}
+  constructor(private serviceService: ServiceService) {
+    this.nmbr = 999;
+    this.displayMeal = 'none';
+  }
 
   ngOnInit(): void {
   }
 
   async bringMealData($event){
-    this.foodObject = await this.serviceService.bringMealbyFood($event.target.id);
-    console.log(this.foodObject);
-    // this.randomMeal = this.foodObject.
+    const foodObject:any = await this.serviceService.bringMealbyFood($event.target.id);
+
+    // First time random number
+    if (this.nmbr === 999) {
+      this.nmbr = Math.floor(Math.random() * ((foodObject.meals.length-1) - 0 + 1) + 0);
+    }
+
+    // Change meal so it doesn't repeat
+    if (this.nmbr === (foodObject.meals.length-1)) {
+      this.nmbr--
+    } else if (this.nmbr >= 0) {
+      this.nmbr++
+    }
+    // Add meal data
+    this.randomMeal = foodObject.meals[this.nmbr];
+    this.food = foodObject.food;
+    this.mealName = this.randomMeal.name;
+    this.ingredients = this.randomMeal.ingredients;
+    this.method = this.randomMeal.method;
+
+    // Display DIV container with meal
+    this.displayMeal = 'block';
   }
 
 
